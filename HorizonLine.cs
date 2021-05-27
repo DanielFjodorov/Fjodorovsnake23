f62788b7fbf339be6c269d7c07274b1971aeadca
opilane <opilane@HP-CZC6297B9B>
2021-05-27 11:54:18 +0300

    Создания линий

---
+++ HorizonLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fjodorovsnake23
+{
+    class HorizonLine
+    {
+        List<Class_Point> pList;
+
+        public HorizonLine(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<Class_Point>();
+            for(int x =xLeft; x <= xReight; x++)
+            {
+                Class_Point p = new Class_Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+  
+        public void Drow()
+        {
+            foreach(Class_Point p in pList)
+            {
+                p.Draw();
+            }
+
+        }
+    }
+}
+
