98a8f93623cca844f7b8b0478eb80a5bb8725578
opilane <opilane@HP-CZC6297B9B>
2021-05-27 13:56:39 +0300

    УРОК 13-14 Создал класс Walls

--- FoodCreator.cs
+++ FoodCreator.cs
@@ -8,5 +8,25 @@ namespace Fjodorovsnake23
 {
     class FoodCreator
     {
+        int mapWidth;
+        int mapHeight;
+        char sym;
+
+
+        Random random = new Random();
+
+        public FoodCreator(int mapWidth, int mapHeight, char sym)
+        {
+            this.mapWidth = mapWidth;
+            this.mapHeight = mapHeight;
+            this.sym = sym;
+        }
+        public Class_Point CreateFood()
+        {
+            int x = random.Next(2, mapWidth - 2);
+            int y = random.Next(2, mapHeight - 2);
+            return new Class_Point(x, y, sym);
+
+        }
     }
 }
